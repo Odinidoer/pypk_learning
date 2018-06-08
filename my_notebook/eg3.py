f2f8a47f7843c1ca109628383a77487a7b9cc1ee
odinidoer <1031091157@qq.com>
2018-06-08 13:02:09 +0800

    在公司把自己写的ipynb，莫烦，和第三方的都放在一起，而且在pychram里面打开传输。以后gitee只放那个res吧

--- my_notebook/eg3.py
+++ my_notebook/eg3.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+#test for gs.desk
+
 def add_layer(inputs, in_size, out_size, activation_function=None):
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
